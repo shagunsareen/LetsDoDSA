c3dbb22cd39176575b2b2079bf580a3196bf82b0
Shagun Sareen <shagunsareen3@gmail.com>
2022-03-11 17:01:10 +0530

    Pepcoding - Day 14

---
+++ src/dsa/with/shagun/leetcode/linkedlist/RemoveDuplicates.java
@@ -0,0 +1,45 @@
+/*
+package dsa.with.shagun.leetcode.linkedlist;
+
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ *//*
+
+public class RemoveDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode dummyNode = new ListNode(0, head);
+
+        ListNode previous = dummyNode;
+
+        while(head!=null)
+        {
+            if(head.next !=null && head.val == head.next.val)
+            {
+                while(head.next!=null && head.val == head.next.val)
+                {
+                    head = head.next;
+                }
+
+                previous.next = head.next;
+            }
+            else
+            {
+                previous = previous.next;
+            }
+
+            head = head.next;
+        }
+
+        return dummyNode.next;
+    }
+}
+*/
