09da7365584660c3f00a1664a50dfc26240a261a
Shagun Sareen <shagunsareen3@gmail.com>
2022-04-02 20:06:45 +0530

    Leetcode - Day 19 - Two Pointer | Reverse A String | 344

---
+++ src/dsa/with/shagun/leetcode/String/ReverseAString.java
@@ -0,0 +1,17 @@
+package dsa.with.shagun.leetcode.String;
+
+public class ReverseAString { //TC : O(n) SC: O(1)
+
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println(s);
+    }
+}
