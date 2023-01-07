6e43c31c5e2dc72f554a844ccdbc31ee17b4abf7
Shagun Sareen <shagunsareen3@gmail.com>
2023-01-07 18:09:02 +0530

    Day 36 - Sorting1 | Advanced

--- src/dsa/with/shagun/scaler/Sorting/LargestNumber.java
+++ src/dsa/with/shagun/scaler/Sorting/LargestNumber.java
@@ -6,7 +6,8 @@ import java.util.stream.IntStream;
 
 public class LargestNumber {
     public static void main(String[] args) {
-        System.out.println(largestNumber(new int[]{2, 3, 9, 0}));
+        //System.out.println(largestNumber(new int[]{2, 3, 9, 0}));
+        System.out.println(largestNumber(new int[]{8, 89}));
     }
 
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
@@ -18,11 +19,21 @@ public class LargestNumber {
         Arrays.sort(arr, new myComparator());
 
         //return Arrays.toString(arr).replaceAll("[\\[\\],]", "");
-        return Arrays.toString(arr).
+        /*return Arrays.toString(arr).
                 replace(",", "").
                 replace("[", "").
                 replace("]", "").
-                replace(" ", "");
+                replace(" ", "");*/
+        StringBuilder res = new StringBuilder("");
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+            res.append(i);
+        }
+        if (sum == 0) {
+            return "0";
+        }
+        return res.toString();
     }
 
     static class myComparator implements Comparator<Integer> {
@@ -30,8 +41,11 @@ public class LargestNumber {
         @Override
         public int compare(Integer val1, Integer val2) {
 
-            Integer one = Integer.valueOf("" + val1 + val2);
-            Integer two = Integer.valueOf("" + val2 + val1);
+            String a = "" + val1 + val2;
+            String b = "" + val2 + val1;
+
+            Long one = Long.parseLong(a);
+            Long two = Long.parseLong(b);
 
             if ((two > one) || two.equals(one)) {
                 return 1;
