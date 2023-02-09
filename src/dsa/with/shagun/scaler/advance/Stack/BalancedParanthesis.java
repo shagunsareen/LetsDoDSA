6144d7b207f9945c4bcca347a39e892390b0a63f
Shagun Sareen <shagunsareen3@gmail.com>
2023-02-09 20:52:43 +0530

    Day 52 - Stacks 1| BalancedParanthesis

---
+++ src/dsa/with/shagun/scaler/advance/Stack/BalancedParanthesis.java
@@ -0,0 +1,30 @@
+package dsa.with.shagun.scaler.advance.Stack;
+
+import java.util.Stack;
+
+public class BalancedParanthesis {
+    public static void main(String[] args) {
+        System.out.println(solve("{([])}"));
+    }
+
+    public static int solve(String A) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (char ch : A.toCharArray()) {
+
+            if (!stack.empty() && stack.peek() == '{' && ch == '}'
+                    || !stack.empty() && stack.peek() == '(' && ch == ')'
+                    || !stack.empty() && stack.peek() == '[' && ch == ']') {
+                stack.pop();
+            } else {
+                //either stack is empty or characters don't match
+                stack.push(ch);
+            }
+        }
+
+        if (stack.size() != 0) {
+            return 0;
+        }
+        return 1;
+    }
+}
